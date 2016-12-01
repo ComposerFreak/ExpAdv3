430f8f276eceb68541b37f415e5524fa6d54e62c
Christopher Goluch <Rusketh@gmail.com>
2016-12-01 15:34:25 +0000

    Added a better benchmarking system, huge thanks to @JazzelHawk

--- lua/entities/wire_expression3_base/sh_context.lua
+++ lua/entities/wire_expression3_base/sh_context.lua
@@ -104,22 +104,23 @@ end
 	Measure: 1000th's of a second.
 ]]
 
-local soft, hard, len;
+local len = 100;
+local soft, hard;
 
 if (SERVER) then
-	soft = CreateConVar("e3_hardquota", 120, { FCVAR_REPLICATED }, "Absolute max usage quota per one tick.");
+	soft = CreateConVar("e3_hardquota", 500, { FCVAR_REPLICATED }, "Absolute max usage quota per one tick.");
 	hard = CreateConVar("e3_softquota", 100, { FCVAR_REPLICATED }, "The max average usage quota.");
-	len = CreateConVar("e3_maxbuffersize", 100, { FCVAR_REPLICATED }, "Window width of the CPU time quota moving average.");
+	--len = CreateConVar("e3_maxbuffersize", 100, { FCVAR_REPLICATED }, "Window width of the CPU time quota moving average.");
 end
 
 if (CLIENT) then
-	soft = CreateClientConVar("e3_hardquota", 120, false, false);
+	soft = CreateClientConVar("e3_hardquota", 500, false, false);
 	hard = CreateClientConVar("e3_softquota", 100, false, false); 
-	len = CreateClientConVar("e3_maxbuffersize", 100, false, false);
+	--len = CreateClientConVar("e3_maxbuffersize", 100, false, false);
 end
 
 function CONTEXT:MaxSampleSize()
-	return len:GetInt() or 3;
+	return len; -- len:GetInt();
 end
 
 function CONTEXT:GetSoftQuota()
@@ -132,25 +133,63 @@ end
 
 --
 
-function CONTEXT:UpdateQuotaValues()
-	-- print("update quota values")
-	if (self.status) then
-		self.cpu_average = (self.cpu_average * 0.95) + (self.cpu_total * 0.05);
-
-		local samples, size = self.cpu_samples, #self.cpu_samples
+function CONTEXT:AddSample(sample)
+	local samples, size = self.cpu_samples, #self.cpu_samples
 		
-		if (size >= self:MaxSampleSize()) then
-			for i = 1, size do
-				samples[i] = samples[i + 1];
-			end -- Move all samples down 1.
+	if (size >= self:MaxSampleSize()) then
+		for i = 1, size do
+			samples[i] = samples[i + 1];
+		end -- Move all samples down 1.
+	end
+
+	samples[size] = sample;
+
+	return size;
+end
+
+function CONTEXT:GetBufferAverage()
+	local average = 0;
+	local samples = #self.cpu_samples;
+
+	for i = 1, samples do
+		average = average + self.cpu_samples[i];
+	end
+
+	return average / samples;
+end
+
+function CONTEXT:GetBufferVariance(average)
+	local average = average or self:GetBufferAverage();
+
+	local sum = 0;
+	local samples = #self.cpu_samples;
+
+	for i = 1, samples do
+		sum = sum + (self.cpu_samples[i] - average) ^ 2;
+	end
+
+	return sum / (samples - 1);
+end
+
+--
+
+
+function CONTEXT:UpdateQuotaValues()
+	if (self.status) then
+
+		local average = self:GetBufferAverage()
+
+		local hard = self:GetHardQuota();
+		
+		if self.cpu_warning then
+			if self.cpu_total < hard * 0.75 then
+				self.cpu_warning = false;
+			end
 		end
 
-		samples[size] = self.cpu_average;
-
-		-- TODO: Perform test on samples?
-
 		self.cpu_total = 0;
-		self.cpu_warning = false;
+
+		self.cpu_average = average;
 	end
 
 	if (r and self.update) then
@@ -165,21 +204,46 @@ end
 local bJit, fdhk, sdhk, ndhk;
 
 function CONTEXT:PreExecute()
-	local cpuMarker = SysTime() - self.cpu_total;
+
+	local cpuMarker = SysTime();
+
+	-- http://www.usablestats.com/calcs/tinv
+	-- Degrees of Freedom = BufferN - 1
+	-- One-sided
+	-- Proportion of Area = 1 - x where x is a percentage that represents a level of significance. 
+	-- A higher significance means it is harder to quota but you are more sure that the limit has been exceeded.
+	-- A lower significance means it is easier to quota but you are less sure that the limit has been exceeded.
+	-- The default value for x is 0.99
+	local criticalValue = 2.3646;
 
 	local cpuCheck = function()
-		self.cpu_total = SysTime() - cpuMarker;
+		local dt = SysTime() - cpuMarker;
 
-		local usage = self:GetHardQuota() / self.cpu_total;
+		local samples = self:AddSample(dt);
 
-		if (usage > 1) then
-			debug.sethook(nil);
-			self:Throw("CPU Quota exceeded");
-		elseif (usage > self:GetsoftQuota()) then
+		local average = self:GetBufferAverage();
+
+		local variance = self:GetBufferVariance(average);
+
+		local soft = self:GetSoftQuota();
+
+		local hard = self:GetHardQuota();
+
+		local statistic = (average - soft) / math.sqrt(variance / samples);
+
+		self.cpu_total = self.cpu_total + dt;
+
+		if statistic > criticalValue then
+			debug.sethook( nil );
+			self:Throw( "Soft CPU Quota Exceeded!");
+		elseif self.cpu_total > hard then
+			debug.sethook( nil );
+			self:Throw( "Hard CPU Quota Exceeded!");
+		elseif self.cpu_total > hard * 0.75 then
 			self.cpu_warning = true;
-		else
-			self.cpu_warning = false;
 		end
+
+		cpuMarker = SysTime();
 	end
 
 	bJit = jit.status();
