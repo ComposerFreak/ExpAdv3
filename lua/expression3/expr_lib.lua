ff62cb05165e950f01a2492c772ad76af6ae3c4e
Christopher Goluch <Rusketh@gmail.com>
2016-12-02 22:10:46 +0000

    fixed a derp with a hook.

--- lua/expression3/expr_lib.lua
+++ lua/expression3/expr_lib.lua
@@ -951,7 +951,7 @@ function EXPR_LIB.Initalize()
 	end
 
 	hook.Run("Expression3.PostRegisterExtensions");
-
+	
 	include("expression3/tokenizer.lua");
 	include("expression3/parser.lua");
 	include("expression3/compiler.lua");
