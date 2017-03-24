d2db997af397d9393e9f73c631f0e12ba42968bb
DaDamRival <dadude770@gmail.com>
2017-03-24 20:03:25 +0100

    Fixed the mess that was angle

--- lua/expression3/extensions/angle.lua
+++ lua/expression3/extensions/angle.lua
@@ -19,17 +19,45 @@ local extension = EXPR_LIB.RegisterExtension("angle")
 extension:RegisterClass("a", {"angle"}, isangle, IsValid)
 
 extension:RegisterConstructor("a", "n,n,n", Angle, true)
+extension:RegisterConstructor("a", "n", function(n) return Angle(n, n, n) end, true)
+extension:RegisterConstructor("a", "", function() return Angle(0, 0, 0) end, true)
 
 --[[
 	Operators
 ]]
 
-extension:RegisterOperator("==", "a,a", "b", 1, nil)
+extension:RegisterOperator("eq", "a,a", "b", 1, nil)
 
-extension:RegisterOperator("+", "a,a", "a", 1, nil, nil);
-extension:RegisterOperator("-", "a,a", "a", 1, nil, nil);
-extension:RegisterOperator("*", "a,a", "a", 1, nil, nil);
-extension:RegisterOperator("/", "a,a", "a", 1, function(a, b)
+extension:RegisterOperator("neq", "a,a", "b", 1, nil)
+
+extension:RegisterOperator("lth", "a,a", "b", 1, function(a, b)
+	return (a.p < b.p) and (a.y < b.y) and (a.r < b.r);
+end, true);
+
+extension:RegisterOperator("leg", "a,a", "b", 1, function(a, b)
+	return (a.p <= b.p) and (a.y <= b.y) and (a.r <= b.r);
+end, true);
+
+extension:RegisterOperator("gth", "a,a", "b", 1, function(a, b)
+	return (a.p > b.p) and (a.y > b.y) and (a.r > b.r);
+end, true);
+
+extension:RegisterOperator("geq", "a,a", "b", 1, function(a, b)
+	return (a.p >= b.p) and (a.y >= b.y) and (a.r >= b.r);
+end, true);
+
+--[[
+]]
+
+extension:RegisterOperator("add", "a,a", "a", 1, nil, nil);
+
+extension:RegisterOperator("sub", "a,a", "a", 1, nil, nil);
+
+extension:RegisterOperator("mul", "a,a", "a", 1, function(a, b)
+	return Angle(a.p * b.p, a.y * b.y, a.r * b.r);
+end, true);
+
+extension:RegisterOperator("div", "a,a", "a", 1, function(a, b)
 	return Angle(a.p / b.p, a.y / b.y, a.r / b.r);
 end, true);
 
@@ -37,32 +65,80 @@ end, true);
 	Methods
 ]]
 
+extension:RegisterAtribute("a", "p", "n");
+extension:RegisterAtribute("a", "y", "n");
+extension:RegisterAtribute("a", "r", "n");
+
 extension:RegisterMethod("a", "isValid", "", "b", 1, function(a)
 	return IsValid(a)
 end, true)
 
-extension:RegisterMethod("a", "forward", "", "v", 1, "Forward")
-extension:RegisterMethod("a", "up", "", "v", 1, "Up")
-extension:RegisterMethod("a", "right", "", "v", 1, "Right")
+--[[
+]]
+
+extension:RegisterMethod("a", "forward", "", "v", 1, Forward)
+extension:RegisterMethod("a", "up", "", "v", 1, Up)
+extension:RegisterMethod("a", "right", "", "v", 1, Right)
+
+--[[
+]]
+
+extension:RegisterMethod("a", "getP", "", "n", 1, function(a)
+	return a.p
+end, true)
+
+extension:RegisterMethod("a", "getY", "", "n", 1, function(a)
+	return a.y
+end, true)
+
+extension:RegisterMethod("a", "getR", "", "n", 1, function(a)
+	return a.r
+end, true)
+
+extension:RegisterMethod("a", "getPYR", "", "n", 3, function(a)
+	return a.p, a.y, a.r
+end, true)
 
 extension:RegisterMethod("a", "unpack", "", "n", 3, function(a)
 	return a.p, a.y, a.r
 end, true)
 
-extension:RegisterMethod("a", "getP", "", "n", 1, function(a) return a.p end, true)
-extension:RegisterMethod("a", "getY", "", "n", 1, function(a) return a.y end, true)
-extension:RegisterMethod("a", "getR", "", "n", 1, function(a) return a.r end, true)
+--[[
+]]
 
-extension:RegisterMethod("a", "setP", "n", "", 0, function(a, n) a.p = n end, true)
-extension:RegisterMethod("a", "setY", "n", "", 0, function(a, n) a.y = n end, true)
-extension:RegisterMethod("a", "setR", "n", "", 0, function(a, n) a.r = n end, true)
+extension:RegisterMethod("a", "setP", "n", "", 0, function(a,n)
+	a.p = n
+end, true)
 
-extension:RegisterMethod("a", "withP", "n", "a", 1, function(a, n) return Angle(n, p.y, p.r) end, true)
-extension:RegisterMethod("a", "withY", "n", "a", 1, function(a, n) return Angle(a.p, n, p.r) end, true)
-extension:RegisterMethod("a", "withR", "n", "a", 1, function(a, n) return Angle(a.p, a.y, n) end, true)
+extension:RegisterMethod("a", "setY", "n", "", 0, function(a,n)
+	a.y = n
+end, true)
 
-extension:RegisterMethod("a", "rotate", "v,n", "", 0, "RotateAroundAxis")
-extension:RegisterMethod("a", "normalize", "", "", 0, "Normalize")
+extension:RegisterMethod("a", "setR", "n", "", 0, function(a,n)
+	a.r = n
+end, true)
+
+--[[
+]]
+
+extension:RegisterMethod("a", "withP", "n", "a", 1, function(a,n)
+	return Angle(n, p.y, p.r)
+end, true)
+
+extension:RegisterMethod("a", "withY", "n", "a", 1, function(a,n)
+	return Angle(a.p, n, p.r)
+end, true)
+
+extension:RegisterMethod("a", "withR", "n", "a", 1, function(a,n)
+	return Angle(a.p, a.y, n)
+end, true)
+
+--[[
+]]
+
+extension:RegisterMethod("a", "rotate", "v,n", "", 0, RotateAroundAxis)
+
+extension:RegisterMethod("a", "normalize", "", "", 0, Normalize)
 
 extension:RegisterMethod("a", "ceil", "", "", 0, function(a)
 	a.p = math.ceil(a.p)
@@ -76,10 +152,16 @@ extension:RegisterMethod("a", "floor", "", "", 0, function(a)
 	a.r = math.floor(a.r)
 end, true)
 
-extension:RegisterMethod("a", "round", "n", "", 0, function(a)
+extension:RegisterMethod("a", "round", "n", "", 0, function(a,n)
 	a.p = math.Round(a.p, n)
 	a.y = math.Round(a.y, n)
 	a.r = math.Round(a.r, n)
+end, true)
+
+extension:RegisterMethod("a", "round", "", "", 0, function(a)
+	a.p = math.Round(a.p)
+	a.y = math.Round(a.y)
+	a.r = math.Round(a.r)
 end, true)
 
 --[[
